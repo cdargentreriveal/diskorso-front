c807c1ac8ff569db47cdd42a4813e7f5ef214d85
Florian Bridoux <bridoux.florian@gmail.com>
2023-02-06 16:29:43 +0100

    integration promenade

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,7 +3,23 @@
     <div class="footer text-white py-10">
       <div class="container mx-auto -md:px-5">
         <div class="flex items-center">
-          Promenades numériques - Tous droits réservés - 2023
+          <div class="w-4/12 text-center">
+            <span>Promenades numériques - Tous droits réservés - 2023</span>
+          </div>
+          <div class="w-3/12 text-center underline">
+            <span>Politique de protection des données</span>
+          </div>
+          <div class="w-2/12 text-center underline">
+            <span>Mention légales</span>
+          </div>
+          <div class="w-3/12 text-center">
+            <span
+              >Réalisé avec amour par
+              <a href="https://www.riveal.fr" target="_blank" class="underline"
+                >Riveal</a
+              ></span
+            >
+          </div>
         </div>
       </div>
     </div>
