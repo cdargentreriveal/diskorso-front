35415895df805cc8a6adf0793e421a0b8cb54d5d
Florian Bridoux <bridoux.florian@gmail.com>
2023-02-09 14:19:14 +0100

    integration promenade

--- pages/promenades/search/[slug]/index.vue
+++ pages/promenades/search/[slug]/index.vue
@@ -228,7 +228,7 @@ const filteredPromenadesByUser = computed(() => {
             <span class="text-xl font-bold purple-color">{{
               promenades.length
             }}</span>
-            <span v-if="promenades.length == 1"> résultat </span>
+            <span v-if="promenades.length <= 1"> résultat </span>
             <span v-else> résultats </span>pour la recherche
             <span class="text-lg italic">“{{ route.params.slug }}“</span>
           </p>
