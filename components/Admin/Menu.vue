2706b47d5698cea1e7e6298aa76d2887ce47d8a4
Florian Bridoux <bridoux.florian@gmail.com>
2023-02-15 13:26:24 +0100

    integration promenade

--- components/Admin/Menu.vue
+++ components/Admin/Menu.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="menu fixed left-0 top-0 h-[100vh] bg-white w-[300px] box-shaddow">
+  <div class="menu fixed left-0 top-0 h-[100vh] bg-white w-[280px] box-shaddow">
     <div class="menu-admin pt-[150px] pb-[25px] h-full">
       <ul class="divide-y divide-slate-300">
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/eye.svg" alt=""
           /></span>
           <span>Tutoriel</span>
         </li>
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/promenade.svg" alt=""
           /></span>
           <span>Mes promenades</span>
         </li>
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/create.svg" alt=""
           /></span>
           <span>Créer une promenade</span>
         </li>
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/extrait.svg" alt=""
           /></span>
@@ -28,13 +28,13 @@
         </li>
       </ul>
       <ul class="divide-y divide-slate-300">
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/compte.svg" alt=""
           /></span>
           <span>Mon compte</span>
         </li>
-        <li class="p-8 flex items-center gap-2">
+        <li class="p-8 flex items-center gap-2 text-sm">
           <span
             ><img src="@/assets/images/icons/menu-admin/deconnexion.svg" alt=""
           /></span>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -13,17 +13,24 @@ onMounted(() => {
 <template>
   <AdminMenu />
   <div class="container mx-auto">
-    <div class="w-8/12 mx-auto pt-[150px]">
-      <div class="py-8">
+    <div class="w-9/12 mx-auto pt-[150px]">
+      <div class="pt-8 flex items-center justify-between">
         <div class="title uppercase font-bold text-3xl -md:text-3xl">
           <h2>Mes <span class="font-medium purple-color">promenades</span></h2>
         </div>
+        <div class="action-button px-4 py-3 text-sm rounded-md text-white">
+          <button>
+            <span></span>
+            <span>Créer une promenade</span>
+          </button>
+        </div>
       </div>
+      <Separator />
     </div>
   </div>
 </template>
 <style scoped lang="scss">
-.btn-submit {
-  background-color: #f55a78;
+.action-button {
+  background-color: var(--purple-color);
 }
 </style>
