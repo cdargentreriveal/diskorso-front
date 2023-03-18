cf42a152a9b369722e94fee9fff315caf2395f3f
Florian Bridoux <bridoux.florian@gmail.com>
2023-03-18 16:11:32 +0100

    integreation projet tempo

---
+++ components/Admin/SearchBar.vue
@@ -0,0 +1,80 @@
+<script lang="ts" setup>
+const searchTag = ref('')
+</script>
+
+<template>
+  <div class="search-bar-container w-full">
+    <div
+      class="search-bar mt-5 mb-5 flex items-center w-9/12 -xl:w-10/12 -sm:w-full mx-auto h-[50px]"
+    >
+      <div class="search-bar-input w-full h-full">
+        <input
+          v-model="searchTag"
+          type="search"
+          placeholder="Rentrer un mot clé pour lancer la recherche..."
+          class="py-4 px-8 w-full h-full border-gray border text-sm italic"
+          @keyup.enter="searchTag.length >= 3 && searchByTag(searchTag)"
+        />
+      </div>
+      <div
+        v-if="searchTag === '' || searchTag.length < 3"
+        :class="
+          searchTag === '' || searchTag.length < 3
+            ? 'disabled search-bar-button text-white text-sm h-full'
+            : ''
+        "
+      >
+        <button class="px-8 w-full h-full uppercase">
+          <span class="flex items-center">Rechercher</span>
+        </button>
+      </div>
+      <div v-else class="search-bar-button text-white text-sm h-full">
+        <NuxtLink :to="`#`">
+          <button class="px-8 w-full h-full uppercase">
+            <span class="flex items-center">Rechercher</span>
+          </button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.search-bar-input input {
+  border-radius: 9999px 0 0 9999px;
+  color: var(--gray-color);
+  &:focus {
+    outline: none;
+  }
+}
+.search-bar-button button {
+  background-color: var(--blue-color);
+  border-radius: 0 9999px 9999px 0;
+  border: none;
+  & :before {
+    content: '';
+    height: 20px;
+    width: 20px;
+    background-image: url('@/assets/images/icons/Icon-search.svg');
+    display: inline-block;
+    margin-right: 0.5rem;
+  }
+}
+.search-bar-button {
+  a {
+    display: inline-block;
+    height: 100%;
+    border-radius: 0 9999px 9999px 0;
+  }
+  & :hover {
+    background-color: #4e8ca5;
+  }
+}
+.disabled {
+  pointer-events: none;
+  button {
+    cursor: not-allowed;
+    background-color: var(--gray-light-color);
+  }
+}
+</style>
--- pages/admin/gestion-utilisateurs/index.vue
+++ pages/admin/gestion-utilisateurs/index.vue
@@ -1,4 +1,12 @@
 <script lang="ts" setup>
+const selectedOption = ref('Selectionner')
+const showMenu = ref(false)
+
+const selectOption = (option: string) => {
+  selectedOption.value = option
+  showMenu.value = false
+}
+
 definePageMeta({
   layout: 'userconnected',
 })
@@ -24,24 +32,178 @@ onBeforeUnmount(() => {
       title-purple="utilisateurs"
       action-btn="Publier"
     />
-    <div class="container_promenade w-9/12 mx-auto flex gap-8"></div>
+    <div class="container_promenade w-9/12 mx-auto">
+      <AdminSearchBar />
+      <div class="users-tab my-10">
+        <div class="flex items-center text-sm">
+          <div class="checkbox mr-10 flex">
+            <input type="checkbox" class="h-[25px] w-[25px]" />
+          </div>
+          <div class="justify-around flex w-full">
+            <div
+              class="bg-white w-full border border-slate-300 text-center py-1"
+            >
+              Nom
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1"
+            >
+              Prénom
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1"
+            >
+              Username
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1"
+            >
+              Email
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1"
+            >
+              Status
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1"
+            >
+              Actif
+            </div>
+          </div>
+        </div>
+        <!--  users -->
+        <div class="flex items-center text-xs my-5 text-slate-500">
+          <div class="checkbox mr-10 flex">
+            <input type="checkbox" class="h-[25px] w-[25px]" />
+          </div>
+          <div class="justify-around flex w-full">
+            <div
+              class="bg-white w-full border border-slate-300 text-center py-1 flex items-center justify-center"
+            >
+              Bridoux
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1 flex items-center justify-center"
+            >
+              Florian
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1 flex items-center justify-center"
+            >
+              FB0305
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1 flex items-center justify-start overflow-y-auto"
+            >
+              bridoux.florian@gmail.comccddcdcdcdfffffffd
+            </div>
+            <div
+              class="bg-white w-full border border-slate-300 text-center p-1 flex items-center justify-center"
+            >
+              <span class="actif inline-flex items-center">Actif</span>
+            </div>
+            <div class="bg-white w-full border border-slate-300 p-1">
+              <div>
+                <div class="relative dropdown">
+                  <button
+                    type="button"
+                    class="flex items-center mx-auto"
+                    @click="showMenu = !showMenu"
+                  >
+                    {{ selectedOption }}
+                    <svg
+                      class="-mr-1 ml-2 h-5 w-5"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        fill-rule="evenodd"
+                        d="M6.293 6.293a1 1 0 011.414 0L10 8.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <div
+                    v-if="showMenu"
+                    class="divide-y divide-slate-300 absolute z-50 top-full py-2 -left-2 mt-2 bg-white rounded-md shadow-lg w-48 ring-1 ring-black ring-opacity-5 focus:outline-none"
+                    role="menu"
+                    aria-orientation="vertical"
+                    aria-labelledby="options-menu"
+                  >
+                    <a
+                      href="#"
+                      class="block items-center px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 hover:text-gray-900 actif"
+                      role="menuitem"
+                      @click.prevent="selectOption('Actif')"
+                    >
+                      Actif
+                    </a>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 hover:text-gray-900 bannir"
+                      role="menuitem"
+                      @click.prevent="selectOption('Bannir')"
+                    >
+                      Bannir
+                    </a>
+                    <a
+                      href="#"
+                      class="block px-4 py-2 text-xs text-gray-700 hover:bg-gray-100 hover:text-gray-900 delete"
+                      role="menuitem"
+                      @click.prevent="selectOption('Supprimer')"
+                    >
+                      Supprimer
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <style scoped lang="scss">
-.action-button {
-  background-color: var(--purple-color);
+.actif {
+  &:after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 5rem;
+    background-color: #50d6b7;
+    margin-left: 8px;
+  }
 }
-sup {
+::-webkit-scrollbar {
+  display: none;
+}
+.dropdown .actif {
+  color: #50d6b7;
+}
+.dropdown .bannir {
+  color: #FBB369;
+  &:after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 5rem;
+    background-color: #50d6b7;
+    margin-left: 8px;
+  }
+}
+.dropdown .delete {
   color: #f55a78;
-}
-.promenade_btns {
-  background-color: #f8f8f8;
-}
-.promenade_btn_image {
-  background-color: var(--blue-color);
-}
-.promenade_btn_transition,
-.extrait_btn {
-  background-color: var(--purple-color);
+  &:after {
+    content: '';
+    width: 8px;
+    height: 8px;
+    border-radius: 5rem;
+    background-color: #50d6b7;
+    margin-left: 8px;
+  }
 }
 </style>
