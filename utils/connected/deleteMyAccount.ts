f2e877d0a719246390449084980d8e8de1983ad2
cdargentre <cdargentre@gmail.com>
2023-05-18 12:19:23 +0200

    method pr suppression compte

---
+++ utils/connected/deleteMyAccount.ts
@@ -0,0 +1,31 @@
+import { refreshToken } from '../connected/refreshToken'
+
+export async function deleteMyAccount(baseURL: string) {
+  try {
+    const xsrfToken = localStorage.getItem('xsrfToken')
+    const options = {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${xsrfToken}`,
+      },
+      credentials: 'include' as RequestCredentials,
+    }
+    const response = await fetch(
+      `${baseURL}/user/user-connected/delete-account`,
+      options
+    )
+    const res = await response.json()
+    if (res.statusCode === 401) {
+      await refreshToken(baseURL)
+      await fetch(`${baseURL}/user/user-connected/delete-account`, options)
+    }
+    if (res.ok) {
+      return res
+    } else {
+      return 'Error'
+    }
+  } catch (error) {
+    return error
+  }
+}
