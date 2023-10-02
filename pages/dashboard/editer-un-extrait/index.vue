a69089a4bbbcfe9729bd64a66f2ae29e209e8aea
cdargentre <cdargentre@gmail.com>
2023-10-02 16:48:41 +0200

    suppression max length

--- pages/dashboard/editer-un-extrait/index.vue
+++ pages/dashboard/editer-un-extrait/index.vue
@@ -85,7 +85,6 @@ const onInputSource = (event: Event) => {
                 type="text"
                 name="scales"
                 class="my-2 p-2 text-sm border border-slate-300 rounded w-full h-[40px]"
-                maxlength="40"
                 @input="onInputSource"
               />
             </div>
