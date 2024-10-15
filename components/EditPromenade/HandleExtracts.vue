a3c99fa4e21f6057cac508d6578dbf585b718829
cdargentre <cdargentre@gmail.com>
2024-10-15 15:11:58 +0200

    augmentation extraots

--- components/EditPromenade/HandleExtracts.vue
+++ components/EditPromenade/HandleExtracts.vue
@@ -33,7 +33,7 @@ function addExcerptBlock(
   id: number,
   index: number
 ): void {
-  if (PromenadeStore.excerptCountEdit < 4) {
+  if (PromenadeStore.excerptCountEdit < 500) {
     // Vérifier si l'extrait est déjà présent
     nextTick(() => {
       const existingExcerpt = PromenadeStore.itemsEdit.find(
